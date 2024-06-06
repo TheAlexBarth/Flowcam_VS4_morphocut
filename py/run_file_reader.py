5332329d321483b1eb1d2d3e0cded12fc67a6ed6
Alex Barth <abarth1014@gmail.com>
2024-06-06 15:06:25 -0500

    finalize formatter and run 14-15

--- py/run_file_reader.py
+++ py/run_file_reader.py
@@ -6,9 +6,15 @@ from datetime import datetime
 
 # Define the file paths
 dir_path = '/Users/planktonperson/Data/Buskey_FC/SWMP_2014-15_badnames'
-output_tsv_path = './test.tsv'
+output_tsv_path = os.path.join(dir_path, 'morphocut','Ecotaxa','ecotaxa_export.tsv')
 
-# Read and add all run file metadata
+
+#################################
+##
+## Read and add all run file metadata
+##
+################################
+
 
 ## Define the parameters of interest and their corresponding column names in the DataFrame
 parameters_of_interest = {
@@ -22,7 +28,7 @@ parameters_of_interest = {
     'Software': 'acq_software',
     'Magnification': 'acq_magnification',
     'Calibration Factor': 'acq_calib_factor',
-    'Used': 'acq_imgused',
+    'Percentage Used': 'acq_img_perc_used',
     'Total': 'acq_imgtotal',
 }
 
@@ -31,7 +37,6 @@ files = [d for d in os.listdir(os.path.join(dir_path, 'raw'))]
 meta_runsum = pd.DataFrame()
 for file_name in files:
     if file_name.endswith('_run_summary.txt'):
-        print(file_name)
         # Initialize a dictionary to store the extracted values
         extracted_values = {value: None for value in parameters_of_interest.values()}
 
@@ -41,140 +46,131 @@ for file_name in files:
             for line in lines:
                 for param, column_name in parameters_of_interest.items():
                     if param in line:
-                        extracted_values[column_name] = line.split(':')[1].strip()
+                        extracted_values[column_name] = line.split(':', 1)[-1].strip()
 
-        extracted_values['object_run_id'] = re.sub("_run_summary.txt", "", file_name)
+        extracted_values['acq_id'] = re.sub("_run_summary.txt", "", file_name)
 
         # add to df
         meta_runsum = pd.concat([meta_runsum, pd.DataFrame([extracted_values])], ignore_index=True)
 
 
-# Extract additional metadata information
-# did this in a function to just keep scoping not an issue
-# this was copied from a bad old function
-# it's confusing since originally these were directories hence names dirs but now its just ids and I didn't change it
-def sample_infomation(meta_df):
-    ids = meta_df['object_run_id']
-    temp_ids = ids    
-
-    def meta_extractor(reg_code, dirs):
-
-        # initialize new structures
-        codes = []
-        mod_dirs = []
-        #loop through
-        for d in dirs:
-            
-            # codes
-            code_match = re.search(reg_code, d) # pull code
-            code= code_match.group() if code_match else None
-            codes.append(code)
-
-            # remove from list of dirs
-            mod_dir = re.sub(reg_code, "", d)
-            mod_dirs.append(mod_dir)
-
-            del code, mod_dir, code_match
-        
-        return {
-            'codes': codes,
-            'dir_names': mod_dirs
-        }
-    
-
-    # extract site code
-    site_regex = r'(AB|CE|CW|MB|SC)'
-
-    site_data = meta_extractor(site_regex, temp_ids)
-
-    #loop to check for debugs
-    for n in set(site_data['codes']):
-        if n is None:
-            print(f'Site issue {n}')
-
-    # add to data
-    meta_df.insert(1, 'sample_site', site_data['codes']) #add site codes
-    temp_ids = site_data['dir_names'] # update dir names
-
-    # extract date codes
-    date_regex = r'(\d{6})'
-
-    date_data = meta_extractor(date_regex, temp_ids)
-    dates = []
-    for date in date_data['codes']:
-        dates.append(datetime.strptime(date, '%m%d%y').strftime('%Y-%m-%d'))
 
 
-    #loop to check for debugs
-    for n in set(date_data['codes']):
-        if n is None:
-            print(f'date issue {n}')
-
-    meta_df.insert(2, 'sample_date', dates)
-    temp_ids = date_data['dir_names']
-
-    # now create replicate number
-    # this will only work once all other numbers have been removed careful in other applications
-    repl_regex = r'(\d{1})'
-    repl_data = meta_extractor(repl_regex, temp_ids)
-
-    repls = []
-    for rep in repl_data['codes']:
-        if rep is None or rep == "1":
-            repls.append("A")
-        elif rep == "2":
-            repls.append("B")
-        elif rep == "3":
-            repls.append('C')
-        else:
-            print(f" There's a {rep}")
-
-    #loop to check for debugs
-    for n in set(repls):
-        if n is None:
-            print(f'code issue {n}')
-
-    meta_df.insert(3, 'acq_run_replicate', repls)
-    return(meta_df)
-
-
-all_meta = sample_infomation(meta_runsum)
+all_meta = ut.badnames_sampleinfo(meta_runsum)
 
 
 all_meta.to_csv(os.path.join(dir_path, 'metadata', 'meta.csv'))
 
+##############################
+'''
 
-# # read in the date sampling metadata
-# meta_dirnames = pd.read_csv(os.path.join(dir_path, 'metadata','meta_dirnames.csv'))
 
 
-# all_meta = pd.merge(meta_runsum, meta_dirnames, on = 'object_run_id', how = 'left')
 
+Metadata checking steps
+
+
+
+
+'''
+##########################
 
 # read in ecotaxa tsv file
 etx = pd.read_csv(os.path.join(dir_path, 'morphocut','Ecotaxa','ecotaxa_export.tsv'), sep='\t',skiprows=[1])
 
-etx['object_run_id'] = etx['object_id'].str.replace(r'_\d+$','',regex=True)
+# create etx fun file
+etx['acq_id'] = etx['object_id'].str.replace(r'_\d+$','',regex=True)
 
-# check that all object run id's match
-not_in_meta = etx[~etx['object_run_id'].isin(all_meta['object_run_id'])]['object_run_id']
-print(not_in_meta.unique())
+#######
+# First check sampling parameters
+#######
 
-# print('\n___\n')
-# print(list(set(all_meta['object_run_id'] - set(etx['object_run_id']))))
+# any case where trigger mode wasn't used 
 
+bad_modes = all_meta[all_meta['acq_run_mode'] != 'Trigger']['acq_id'].unique()
+print(all_meta)
+if len(bad_modes) > 0:
+    print(f'Bad run modes were found for: {bad_modes}')
+    resp_ok = False
+    while not resp_ok:
+        bad_resp = input('Go ahead and remove file(s) [y/n]: ').strip().lower()
+        if bad_resp == 'y':
+            all_meta = all_meta[all_meta['acq_run_mode'] == 'Trigger']
+            print(all_meta)
+            resp_ok = True
+        elif bad_resp == 'n':
+            raise ValueError("intentional stop")
+        else:
+            print('only y/n response')
+            next
+
+
+############ Remove images from faulty runs #########################################################
+
+not_in_meta = etx[~etx['acq_id'].isin(all_meta['acq_id'])]['acq_id']
+not_in_etx = all_meta[~all_meta['acq_id'].isin(etx['acq_id'])]['acq_id']
+
+# if there's something missing from the etx but in the meta, i need an error
+if(len(not_in_etx) > 0):
+   raise ValueError(f'There is something wrong with {not_in_etx} in the etx file')
+
+# if there's an issue with run files, we need to remove from etx and delete the corresponding data sets
+if(len(not_in_meta) > 0):
+    print('\n')
+    print('_____')
+    print(f'There\'s and issue with these run files {not_in_meta.unique()} this will delete from the etx project')
+    print('\n')
+    resp_ok = False
+    while not resp_ok:
+        resp = input('Go ahead and delete these from project? [y/n]').strip().lower()
+        if resp == 'y':
+            etx = etx[etx['acq_id'].isin(all_meta['acq_id'])]
+            #get all image files
+            img_files = [f for f in os.listdir(os.path.join(dir_path, 'morphocut','Ecotaxa'))]
+            #loop through to fine who's not in the meta
+            counter = 0
+            for file in img_files:
+                for bad in not_in_meta.unique():
+                    if file.startswith(bad):
+                        print('\n')
+                        print(f'Removing image file: {file}')
+                        counter += 1
+                        os.remove(os.path.join(dir_path, 'morphocut','Ecotaxa',file))
+            #confirm it worked
+            print('\n')
+            print(f'Total Removed: {counter}')
+            resp_ok = True
+        elif resp == 'n':
+            raise ValueError('Script intentionally abandoned')
+        else:
+            print('Enter only [y/n]')
+
+
+###################
+'''
+
+
+
+
+Finalize and merge everything
+
+
+
+'''
+###################
 
 # # merge it all togetherf
-# new_etx = pd.merge(etx, all_meta, on = 'object_run_id', how = 'left')
+new_etx = pd.merge(etx, all_meta, on = 'acq_id', how = 'left')
+
 
 # print(new_etx.tail())
-# # new_etx = ut.add_dtype_line(new_etx)
+new_etx = ut.add_dtype_line(new_etx)
 
 # # new_etx.to_csv(output_tsv_path, set = '\t', index = False)
 
-# # df_with_types = ut.add_dtype_line(df)
+# df_with_types = ut.add_dtype_line(df)
 
 # # # Save the DataFrame to a .tsv file
-# new_etx.to_csv(output_tsv_path, sep='\t', index=False)
+new_etx.to_csv(output_tsv_path, sep='\t', index=False)
 
-# # print(f"Data saved to {output_tsv_path}")
+print(f"Data saved to {output_tsv_path}")
--- py/utilities.py
+++ py/utilities.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import numpy as np
 import re
 import os
 import shutil
+from datetime import datetime
 
 # add tsv top line
 def add_dtype_line(df):
     """
     Write top line for ecotaxa tsv format
     """
-
     # Define a function to determine the data type
     def assign_tsv_type(data):
-        if isinstance(data, (int, float, complex)):
+        if isinstance(data, (float)):
             return '[f]'
         else:
             return '[t]'
@@ -19,11 +20,14 @@ def add_dtype_line(df):
     def as_num_if_num(value):
 
         # Check if the string matches a numeric pattern
-        if re.fullmatch(r"^\d+(\.\d+)?$", value):
-            # Convert to float and return
-            return float(value)
+        if isinstance(value, str):
+            if re.fullmatch(r"^\d+(\.\d+)?$", value):
+                # Convert to float and return
+                return float(value)
+            else:
+                # Return the original string
+                return value
         else:
-            # Return the original string
             return value
         
     df_dtypes = df.map(as_num_if_num).map(assign_tsv_type)
@@ -62,3 +66,98 @@ def move_files(source_dir, destination_dir):
         for file in files:
             if not file.startswith('cal_image'):
                 shutil.move(os.path.join(root, file), destination_dir)
+
+
+
+#################
+# Date Extracting Functions
+#################
+
+# Extract additional metadata information
+# did this in a function to just keep scoping not an issue
+# this was copied from a bad old function
+# it's confusing since originally these were directories hence names dirs but now its just ids and I didn't change it
+def badnames_sampleinfo(meta_df):
+    ids = meta_df['acq_id']
+    temp_ids = ids    
+
+    def meta_extractor(reg_code, dirs):
+
+        # initialize new structures
+        codes = []
+        mod_dirs = []
+        #loop through
+        for d in dirs:
+            
+            # codes
+            code_match = re.search(reg_code, d) # pull code
+            code= code_match.group() if code_match else None
+            codes.append(code)
+
+            # remove from list of dirs
+            mod_dir = re.sub(reg_code, "", d)
+            mod_dirs.append(mod_dir)
+
+            del code, mod_dir, code_match
+        
+        return {
+            'codes': codes,
+            'dir_names': mod_dirs
+        }
+    
+
+    # extract site code
+    site_regex = r'(AB|CE|CW|MB|SC)'
+
+    site_data = meta_extractor(site_regex, temp_ids)
+
+    #loop to check for debugs
+    for n in set(site_data['codes']):
+        if n is None:
+            print(f'Site issue {n}')
+
+    # add to data
+    meta_df.insert(1, 'sample_site', site_data['codes']) #add site codes
+    temp_ids = site_data['dir_names'] # update dir names
+
+    # extract date codes
+    date_regex = r'(\d{6})'
+
+    date_data = meta_extractor(date_regex, temp_ids)
+    dates = []
+    for date in date_data['codes']:
+        dates.append(datetime.strptime(date, '%m%d%y').strftime('%Y%m%d'))
+
+
+    #loop to check for debugs
+    for n in set(date_data['codes']):
+        if n is None:
+            print(f'date issue {n}')
+
+    meta_df.insert(2, 'sample_date', dates)
+    temp_ids = date_data['dir_names']
+
+    # now create replicate number
+    # this will only work once all other numbers have been removed careful in other applications
+    repl_regex = r'(\d{1})'
+    repl_data = meta_extractor(repl_regex, temp_ids)
+
+    repls = []
+    for rep in repl_data['codes']:
+        if rep is None or rep == "1":
+            repls.append("A")
+        elif rep == "2":
+            repls.append("B")
+        elif rep == "3":
+            repls.append('C')
+        else:
+            print(f" There's a {rep}")
+
+    #loop to check for debugs
+    for n in set(repls):
+        if n is None:
+            print(f'code issue {n}')
+
+    meta_df.insert(3, 'sample_replicate', repls)
+    meta_df.insert(4, 'sample_id', meta_df['sample_site'] + "_" + meta_df['sample_date'])
+    return(meta_df)
